6f40578a3fbe043df7f1400e67ffaedb8f297ec6
yagelkniz <yageledu@gmail.com>
2026-05-23 21:05:08 -0600

    Fix: add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/HEBREO-HACK/",
   server: {
     host: "::",
     port: 8080,
